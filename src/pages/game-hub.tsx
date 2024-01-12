cacc2a065b3391fda59907782c7f4b41bc6cc379
user530 <m.timarsuev@gmail.com>
2024-01-12 23:40:00 +0300

    Game hub class on selection + clear up

--- src/pages/game-hub.tsx
+++ src/pages/game-hub.tsx
@@ -9,14 +9,13 @@ interface IGameHubPage {
 
 export const GameHubPage: React.FC<IGameHubPage> = (props: IGameHubPage) => {
     const dispatch = useAppDispatch();
-    const playerId = useAppSelector((state) => state.gameData.player)?.playerId;
+    const playerId = useAppSelector((state) => state.gameData.player.playerId)
     const lobbyList = useAppSelector((state) => state.gameData.lobbyList);
+    const [selectedLobbyId, setSelectedLobbyId] = React.useState<null | string>(null);
 
-    let selectedLobbyId: null | string = null;
-
-    const setSelectedLobby = (id: string) => {
-        selectedLobbyId = id;
-        console.log('Lobby selected: ', selectedLobbyId)
+    const lobbyRowClick = (id: string) => {
+        console.log('Lobby selected: ', id)
+        setSelectedLobbyId(id);
     };
 
     const handleQuitHubClick = () => {
@@ -33,19 +32,30 @@ export const GameHubPage: React.FC<IGameHubPage> = (props: IGameHubPage) => {
     const handleJoinGameClick = () => {
         console.log('Join Game btn clicked!');
         if (!playerId || !selectedLobbyId) return;
-        dispatch(sendSocketCommand(createHubJoinMessage({ playerId, lobbyId: '' + selectedLobbyId })));
+        dispatch(sendSocketCommand(createHubJoinMessage({ playerId, lobbyId: selectedLobbyId })));
     };
+
+    React.useEffect(
+        () => {
+            const gameStillActive = lobbyList.reduce((gameStillActive, lobby) => (gameStillActive || (lobby.gameId === selectedLobbyId)), false)
+            if (!gameStillActive)
+                setSelectedLobbyId(null);
+        },
+        [lobbyList]
+    )
 
     return <>
         <div style={{ minWidth: 600, minHeight: 350, backgroundColor: 'gray', padding: 50 }}>
             <div className="lobby-list" style={{ minWidth: 300, minHeight: 200, backgroundColor: 'white' }} >
+                <h2>{selectedLobbyId}</h2>{/* DELETE */}
                 {
                     lobbyList.map(
                         ({ gameId, hostName }) => (
                             <div
                                 key={gameId}
                                 style={{ display: 'flex' }}
-                                onClick={(e) => setSelectedLobby(gameId)}>
+                                className={selectedLobbyId === gameId ? 'active' : ''}
+                                onClick={(e) => lobbyRowClick(gameId)}>
                                 <p>{gameId}</p>
                                 <p>{hostName}</p>
                             </div>

8dca78547d924e9de6cba16b237c5b9699d5ff63
user530 <m.timarsuev@gmail.com>
2024-01-13 19:53:56 +0300

    Game hub page updated to use Lobby item component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { GameInstancePage } from './pages/game-instance';
 import { useAppSelector } from './app/hooks/useStore';
 import { TestLoginPage } from './pages/test-login';
-import { GameHubPage } from './pages/game-hub';
+import { GameHubPage } from './pages/game-hub/game-hub';
 
 function App() {
   const playerId = useAppSelector((state) => state.gameData.player?.playerId);
--- src/pages/game-hub/game-hub.tsx
+++ src/pages/game-hub/game-hub.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useAppDispatch, useAppSelector } from '../app/hooks/useStore';
-import { sendSocketCommand } from '../app/store/reducers/slices/socket-messages.slice';
+import styles from './game-hub.module.css';
+import { LobbyItem } from '../../components/lobby-item/lobby-item';
+import { useAppDispatch, useAppSelector } from '../../app/hooks/useStore';
+import { sendSocketCommand } from '../../app/store/reducers/slices/socket-messages.slice';
 import { createHubHostMessage, createHubJoinMessage, createHubLeaveMessage } from '@user530/ws_game_shared/creators/messages';
 
 interface IGameHubPage {
@@ -14,30 +16,32 @@ export const GameHubPage: React.FC<IGameHubPage> = (props: IGameHubPage) => {
     const [selectedLobbyId, setSelectedLobbyId] = React.useState<null | string>(null);
 
     const lobbyRowClick = (id: string) => {
-        console.log('Lobby selected: ', id)
         setSelectedLobbyId(id);
     };
 
     const handleQuitHubClick = () => {
-        console.log('Quit btn clicked!');
         if (!playerId) return;
+
         dispatch(sendSocketCommand(createHubLeaveMessage()));
     };
 
     const handleHostGameClick = () => {
-        console.log('Host Game btn clicked!');
         if (!playerId) return;
+
         dispatch(sendSocketCommand(createHubHostMessage({ playerId })));
     };
     const handleJoinGameClick = () => {
-        console.log('Join Game btn clicked!');
         if (!playerId || !selectedLobbyId) return;
+
         dispatch(sendSocketCommand(createHubJoinMessage({ playerId, lobbyId: selectedLobbyId })));
     };
 
+    // Clear up selected lobby id if selected game is not open anymore
     React.useEffect(
         () => {
-            const gameStillActive = lobbyList.reduce((gameStillActive, lobby) => (gameStillActive || (lobby.gameId === selectedLobbyId)), false)
+            const gameStillActive = lobbyList.reduce(
+                (gameStillActive, lobby) => (gameStillActive || (lobby.gameId === selectedLobbyId)), false);
+
             if (!gameStillActive)
                 setSelectedLobbyId(null);
         },
@@ -45,26 +49,21 @@ export const GameHubPage: React.FC<IGameHubPage> = (props: IGameHubPage) => {
     )
 
     return <>
-        <div style={{ minWidth: 600, minHeight: 350, backgroundColor: 'gray', padding: 50 }}>
-            <div className="lobby-list" style={{ minWidth: 300, minHeight: 200, backgroundColor: 'white' }} >
-                <h2>{selectedLobbyId}</h2>{/* DELETE */}
+        <div className={styles['hub-wrapper']}>
+            <div className={styles['hub-list']}>
                 {
                     lobbyList.map(
-                        ({ gameId, hostName }) => (
-                            <div
-                                key={gameId}
-                                style={{ display: 'flex' }}
-                                className={selectedLobbyId === gameId ? 'active' : ''}
-                                onClick={(e) => lobbyRowClick(gameId)}>
-                                <p>{gameId}</p>
-                                <p>{hostName}</p>
-                            </div>
-                        )
-                    )
+                        (lobbyData) =>
+                        (<LobbyItem
+                            key={lobbyData.gameId}
+                            onClick={(e) => lobbyRowClick(lobbyData.gameId)}
+                            isActive={selectedLobbyId === lobbyData.gameId}
+                            {...lobbyData}
+                        />))
                 }
             </div>
 
-            <div className='hub-controls'>
+            <div className={styles['hub-controls']}>
                 <button onClick={handleQuitHubClick}>Quit</button>
                 <button onClick={handleHostGameClick}>Host</button>
                 <button onClick={handleJoinGameClick}>Join</button>

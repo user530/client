362a6554343702f287e10c9f726a1adbba1c09c3
user530 <m.timarsuev@gmail.com>
2024-01-13 19:53:12 +0300

    Created lobby item component

---
+++ src/components/lobby-item/lobby-item.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './lobby-item.module.css';
+
+interface ILobbyItem {
+    onClick?: (...args: any[]) => any;
+    className?: string | undefined;
+    isActive: boolean;
+    gameId: string;
+    hostName: string;
+}
+
+export const LobbyItem: React.FC<ILobbyItem> = (props: ILobbyItem) => {
+    const { className, onClick, gameId, hostName, isActive } = props;
+
+    return <div
+        onClick={onClick}
+        className={
+            (className ? className : '') +
+            styles['lobby-item'] +
+            (isActive ? ` ${styles['lobby-item--selected']}` : '')
+        }
+    >
+        <p>{gameId}</p>
+        <p>{hostName}</p>
+    </div>
+}
+

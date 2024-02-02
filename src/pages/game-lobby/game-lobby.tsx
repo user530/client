7e8a02d655f3acb5eced96db38fa7abce1da7ea6
user530 <m.timarsuev@gmail.com>
2024-02-02 23:57:20 +0300

    Game Lobby refactored with template and lobby component

--- src/pages/game-lobby/game-lobby.tsx
+++ src/pages/game-lobby/game-lobby.tsx
@@ -1,91 +1,26 @@
 import React from 'react';
 import styles from './game-lobby.module.css';
-import { useAppDispatch, useAppSelector } from '../../app/hooks/useStore';
-import { sendSocketCommand } from '../../app/store/reducers/slices/socket-messages.slice';
-import { createLobbyKickMessage, createLobbyLeaveMessage, createLobbyStartMessage } from '@user530/ws_game_shared/creators/messages';
+import { AppPageTemplate } from '../app-page-wrapper/app-page-template';
 import { ChatComponent } from '../../components/chat-component/chat.component';
+import { LobbyComponent } from '../../components/lobby-component/lobby.component';
 
 interface IGameLobbyPage {
 
 }
 
 export const GameLobbyPage: React.FC<IGameLobbyPage> = (props: IGameLobbyPage) => {
-
-    const playerId = useAppSelector((state) => state.gameData.player!.playerId)!;
-    const { gameId, guest, host } = useAppSelector((state) => state.gameData.game)!;
-    const dispatch = useAppDispatch();
-
-    const [playerRole] = React.useState<'host' | 'guest'>(
-        playerId === host.hostId
-            ? 'host'
-            : 'guest');
-
-    const handleLeaveLobbyClick = () => {
-        console.log('Handle Leave Lobby Click');
-
-        dispatch(sendSocketCommand(createLobbyLeaveMessage({ playerId, gameId })))
-    };
-
-    const handleKickGuestClick = () => {
-        console.log('Handle Kick Guest Click');
-        console.log(playerRole)
-        console.log(guest)
-        if (playerRole === 'guest' || !guest) return;
-        dispatch(sendSocketCommand(createLobbyKickMessage({ gameId, playerId })));
-    };
-
-    const handleStartGameClick = () => {
-        console.log('Handle Start Game Click');
-        console.log(playerRole)
-        console.log(guest)
-
-        if (playerRole === 'guest' || !guest) return;
-        dispatch(sendSocketCommand(createLobbyStartMessage({ gameId, playerId: host.hostId })));
-    };
-
     return (
         <>
-            <div className={styles['page-wrapper']}>
-                <div className={styles['page-heading']}>
-                    <h1 className={styles['h1']}>Tic-Tac-Toe Game Lobby</h1>
-                </div>
-                <div className={styles['page-main']}>
-                    <div className={styles['page-main__row-1']}>
-                        <div className={styles['lobby-wrapper']}>
-                            <div className={styles['lobby-heading']}>
-                                <h2 className={styles['h2']}>Game#: {gameId}</h2>
-                            </div>
-                            <div className={styles['lobby-body']}>
-                                <div className={styles['column']}>
-                                    <p className={styles['player-name']}>{host.hostName}</p>
-                                    <p className={styles['player-role']}>X</p>
-                                </div>
-                                <div className={styles['column']}>
-                                    <p className={styles['player-name']}>{guest ? guest.guestName : '...'}</p>
-                                    <p className={styles['player-role']}>O</p>
-                                </div>
-                            </div>
-                            <div className={styles['lobby-controls']}>
-                                <button className={styles['lobby-controls__btn']} onClick={handleLeaveLobbyClick}>Leave</button>
-
-                                {
-                                    playerRole === 'host'
-                                        ? <>
-                                            <button className={styles['lobby-controls__btn']} onClick={handleKickGuestClick}>Kick</button>
-                                            <button className={styles['lobby-controls__btn']} onClick={handleStartGameClick}>Start</button>
-                                        </>
-                                        : null
-                                }
-
-                            </div>
-                        </div>
+            <AppPageTemplate>
+                <div className={styles['flex-column']}>
+                    <div className={styles['row-1']}>
+                        <LobbyComponent />
                     </div>
-                    <div className={styles['page-main__row-2']}>
+
+                    <div className={styles['row-2']}>
                         <ChatComponent />
                     </div>
                 </div>
-            </div>
-
-
+            </AppPageTemplate>
         </>)
 }

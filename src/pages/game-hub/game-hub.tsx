61141616c12b2caf591f3ffb577b69518ec70a58
user530 <m.timarsuev@gmail.com>
2024-02-02 23:41:20 +0300

    Hub page reworked utilizing page template

--- src/pages/game-hub/game-hub.tsx
+++ src/pages/game-hub/game-hub.tsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import styles from './game-hub.module.css';
+import { AppPageTemplate } from '../app-page-wrapper/app-page-template';
 import { ChatComponent } from '../../components/chat-component/chat.component';
 import { HubComponent } from '../../components/hub-component/hub.component';
 
-interface IGameHubPage {
-
-}
-
-export const GameHubPage: React.FC<IGameHubPage> = (props: IGameHubPage) => {
-
+export const GameHubPage: React.FC = () => {
 
     return <>
-        <div className={styles['page-wrapper']}>
-            <div className={styles['page-heading']}>
-                <h1 className={styles['h1']}>Tic-Tac-Toe Game App</h1>
-            </div>
-            <div className={styles['page-main']}>
-                <div className={styles['page-main__col-1']}>
+        <AppPageTemplate>
+            <div className={styles['flex']}>
+                <div className={styles['column-main']}>
                     <ChatComponent />
                 </div>
-                <div className={styles['page-main__col-2']}>
+
+                <div className={styles['column-sub']}>
                     <HubComponent />
                 </div>
             </div>
-        </div>
+        </AppPageTemplate>
     </>
 };

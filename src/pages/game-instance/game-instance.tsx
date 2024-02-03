1920b75923e668c3e0566c7a999af97c7d8d054b
user530 <m.timarsuev@gmail.com>
2024-02-03 23:25:12 +0300

    Game Instance page - Initial implementation

--- src/pages/game-instance/game-instance.tsx
+++ src/pages/game-instance/game-instance.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
 import styles from './game-instance.module.css';
-import { GameField } from '../../components/game-field/game-field';
-import { ForfeitGameBtn } from '../../components/forfeit-btn/forfeit-game-btn';
-import { useAppDispatch, useAppSelector } from '../../app/hooks/useStore';
-import { PopUpWindow } from '../../components/pop-up-windows/pop-up-window';
-import { setGame } from '../../app/store/reducers/slices/game-data.slice';
-import { hubSocketConnection } from '../../app/store/reducers/slices/socket-messages.slice';
+import { AppPageTemplate } from '../app-page-wrapper/app-page-template';
+import { ChatComponent } from '../../components/chat-component/chat.component';
+import { GameComponent } from '../../components/game-component/game.component';
 
 interface IGameInstancePage {
 }
 
 
 export const GameInstancePage: React.FC<IGameInstancePage> = (props: IGameInstancePage) => {
-    const popup = useAppSelector((state) => state.gameData.popupWindow);
-
-    const dispatch = useAppDispatch();
-
-    const closeConnection = () => {
-        console.log('CLOSE WS CONNECTION'); //PLACEHOLDER
-        dispatch(setGame(null));
-        dispatch(hubSocketConnection());
-    }
 
     return <>
-        <GameField />
-        <ForfeitGameBtn />
-        {
-            popup === 'win'
-                ? <PopUpWindow type='green' message='You won!' cb={closeConnection} />
-                : popup === 'loose'
-                    ? <PopUpWindow type='red' message='You lost!' cb={closeConnection} />
-                    : popup === 'draw'
-                        ? <PopUpWindow type='yellow' message='Game draw!' cb={closeConnection} />
-                        : null
-        }
+        <AppPageTemplate>
+            <div className={styles['flex-column']}>
+                <div className={styles['row-1']}>
+                    <GameComponent />
+                </div>
+
+                <div className={styles['row-2']}>
+                    <ChatComponent />
+                </div>
+            </div>
+        </AppPageTemplate>
     </>
 } 

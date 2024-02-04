83b41909c3139ba1dfe0ead2a9d7a8597919a536
user530 <m.timarsuev@gmail.com>
2024-02-04 22:28:58 +0300

    Game component a small clean-up

--- src/components/game-component/game.component.tsx
+++ src/components/game-component/game.component.tsx
@@ -2,21 +2,8 @@ import React from 'react';
 import styles from './game.module.css';
 import { GameField } from './game-field/game-field';
 import { ForfeitGameBtn } from './forfeit-btn/forfeit-game-btn';
-import { useAppDispatch, useAppSelector } from '../../app/hooks/useStore';
-import { setGame } from '../../app/store/reducers/slices/game-data.slice';
-import { hubSocketConnection } from '../../app/store/reducers/slices/socket-messages.slice';
 
 export const GameComponent: React.FC = () => {
-    const popup = useAppSelector((state) => state.gameData.popupWindow);
-
-    const dispatch = useAppDispatch();
-
-    const closeConnection = () => {
-        console.log('CLOSE WS CONNECTION'); //PLACEHOLDER
-        dispatch(setGame(null));
-        dispatch(hubSocketConnection());
-    }
-
     return <>
         <div className={styles['game-wrapper']}>
             <div className={styles['game-heading']}>
@@ -29,16 +16,5 @@ export const GameComponent: React.FC = () => {
                 <ForfeitGameBtn />
             </div>
         </div>
-
-        {/* 
-        {
-            popup === 'win'
-                ? <PopUpWindow type='green' message='You won!' cb={closeConnection} />
-                : popup === 'loose'
-                    ? <PopUpWindow type='red' message='You lost!' cb={closeConnection} />
-                    : popup === 'draw'
-                        ? <PopUpWindow type='yellow' message='Game draw!' cb={closeConnection} />
-                        : null
-        } */}
     </>
 }
